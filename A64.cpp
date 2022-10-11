583658f727ce74874e0b22db06124aca6ee76709
kojosh <111550404+kojosh@users.noreply.github.com>
2022-10-10 21:18:32 -0700

    assignment 6-4

--- A64.cpp
+++ A64.cpp
@@ -8,28 +8,59 @@
 //			user input = 40, 20 , call getListprime(   ) // the both parameters are default. [0,100]
 //			user input = 20, 50,  call getListprime(20, 50) // call the function with actual arguments
 
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 //----------
 // Make a line for Function Prototype for getListprime Here
-//----------
+void printPrimeList(int a, int b){
+    int flag;
+    cout << " Prime numbers between " << a << " and " << b << ":" << endl;\
+    for(int i=a; i <=b; i++){
+        if(i == 1 || i == 0)
+           continue;
+        flag =1;
+        for(int j =2; j<= (i/2); ++j){
+        // for(int j =2; j<= (i/j); ++j){
+			// for (int j=2; j<i; j++) 
+            if(i % j == 0){
+                flag =0;
+                break;
+            }
+        } 
+        if (flag == 1){
+            cout << i << " ";
+        }   
+    }
+}
+
+void getListprime(int begin){
+    printPrimeList(begin, 100);
+}
+
+void getListprime(){
+    printPrimeList(0,100);
+}
+
+void getListprime(int begin, int end){
+    printPrimeList(begin, end);
+}
+
 
 int main()
 {
 	int begin, end;
-	cout << "Enter your input (two integer for range ) \n";
+	cout << "Enter your input (two integers for range) \n";
 	cin >> begin >> end;
+    if(begin <= 0 || end <= 0){
+        cout << "Invalid!";
+    }
 	if (begin < end)
 		getListprime(begin, end);
 	else if (begin > end)
 		getListprime();
 	else
 		getListprime(begin);
-}
-void getListprime(int begin, int end)
-{
-
-	// Complete this function
 }
